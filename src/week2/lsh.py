4e7f107d830a1f8b9132ac24d81f8011e010ebe9
Leonardo Braga <leonardokcardozo@gmail.com>
2017-07-18 16:57:27 -0300

    Implementation of LSH from scratch on Wikipedia data

---
+++ src/week2/lsh.py
@@ -0,0 +1,191 @@
+import numpy as np
+import graphlab
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import pairwise_distances
+import time
+from copy import copy
+import matplotlib.pyplot as plt
+
+wiki = graphlab.SFrame('../../data/people_wiki.gl/')
+
+wiki = wiki.add_row_number()
+
+wiki['tf_idf'] = graphlab.text_analytics.tf_idf(wiki['text'])
+
+start=time.time()
+corpus, mapping = sframe_to_scipy(wiki['tf_idf'])
+end=time.time()
+print end-start
+
+def generate_random_vectors(num_vector, dim):
+    return np.random.randn(dim, num_vector)
+
+def train_lsh(data, num_vector=16, seed=None):
+    
+    dim = data.shape[1]
+    if seed is not None:
+        np.random.seed(seed)
+    random_vectors = generate_random_vectors(num_vector, dim)
+  
+    powers_of_two = 1 << np.arange(num_vector-1, -1, -1)
+  
+    table = {}
+    
+    # Partition data points into bins
+    bin_index_bits = (data.dot(random_vectors) >= 0)
+  
+    # Encode bin index bits into integers
+    bin_indices = bin_index_bits.dot(powers_of_two)
+    
+    # Update `table` so that `table[i]` is the list of document ids with bin index equal to i.
+    for data_index, bin_index in enumerate(bin_indices):
+        if bin_index not in table:
+            # If no list yet exists for this bin, assign the bin an empty list.
+            table[bin_index] = []
+        # Fetch the list of document ids associated with the bin and add the document id to the end.
+        table[bin_index].append(data_index) # YOUR CODE HERE
+
+    model = {'data': data,
+             'bin_index_bits': bin_index_bits,
+             'bin_indices': bin_indices,
+             'table': table,
+             'random_vectors': random_vectors,
+             'num_vector': num_vector}
+    
+    return model
+
+# 1
+obama_id = wiki[wiki['name'] == 'Barack Obama']['id'][0]
+obama_id
+
+# 2
+bin_indices = model['bin_indices']
+bin_indices[obama_id]
+
+# 3
+bin_index_bits = model['bin_index_bits']
+
+biden_id = wiki[wiki['name'] == 'Joe Biden']['id'][0]
+
+(bin_index_bits[obama_id] == bin_index_bits[biden_id]).sum()
+
+# 4
+from itertools import combinations
+
+num_vector = 16
+search_radius = 3
+
+for diff in combinations(range(num_vector), search_radius):
+    print diff
+
+def search_nearby_bins(query_bin_bits, table, search_radius=2, initial_candidates=set()):
+    """
+    For a given query vector and trained LSH model, return all candidate neighbors for
+    the query among all bins within the given search radius.
+    
+    Example usage
+    -------------
+    >>> model = train_lsh(corpus, num_vector=16, seed=143)
+    >>> q = model['bin_index_bits'][0]  # vector for the first document
+  
+    >>> candidates = search_nearby_bins(q, model['table'])
+    """
+    num_vector = len(query_bin_bits)
+    powers_of_two = 1 << np.arange(num_vector-1, -1, -1)
+    
+    # Allow the user to provide an initial set of candidates.
+    candidate_set = copy(initial_candidates)
+    
+    for different_bits in combinations(range(num_vector), search_radius):       
+        # Flip the bits (n_1,n_2,...,n_r) of the query bin to produce a new bit vector.
+        ## Hint: you can iterate over a tuple like a list
+        alternate_bits = copy(query_bin_bits)
+        for i in different_bits:
+            alternate_bits[i] = not alternate_bits[i]
+        
+        # Convert the new bit vector to an integer index
+        nearby_bin = alternate_bits.dot(powers_of_two)
+        
+        # Fetch the list of documents belonging to the bin indexed by the new bit vector.
+        # Then add those documents to candidate_set
+        # Make sure that the bin exists in the table!
+        # Hint: update() method for sets lets you add an entire list to the set
+        if nearby_bin in table:
+            candidate_set.update(table[nearby_bin]) # YOUR CODE HERE: Update candidate_set with the documents in this bin.
+            
+    return candidate_set
+
+def query(vec, model, k, max_search_radius):
+  
+    data = model['data']
+    table = model['table']
+    random_vectors = model['random_vectors']
+    num_vector = random_vectors.shape[1]
+    
+    
+    # Compute bin index for the query vector, in bit representation.
+    bin_index_bits = (vec.dot(random_vectors) >= 0).flatten()
+    
+    # Search nearby bins and collect candidates
+    candidate_set = set()
+    for search_radius in xrange(max_search_radius+1):
+        candidate_set = search_nearby_bins(bin_index_bits, table, search_radius, initial_candidates=candidate_set)
+    
+    # Sort candidates by their true distances from the query
+    nearest_neighbors = graphlab.SFrame({'id':candidate_set})
+    candidates = data[np.array(list(candidate_set)),:]
+    nearest_neighbors['distance'] = pairwise_distances(candidates, vec, metric='cosine').flatten()
+    
+    return nearest_neighbors.topk('distance', k, reverse=True), len(candidate_set)
+
+num_candidates_history = []
+query_time_history = []
+max_distance_from_query_history = []
+min_distance_from_query_history = []
+average_distance_from_query_history = []
+
+for max_search_radius in xrange(17):
+    start=time.time()
+    result, num_candidates = query(corpus[35817,:], model, k=10,
+                                   max_search_radius=max_search_radius)
+    end=time.time()
+    query_time = end-start
+    
+    print 'Radius:', max_search_radius
+#    print result.join(wiki[['id', 'name']], on='id').sort('distance')
+    
+    average_distance_from_query = result['distance'][1:].mean()
+    max_distance_from_query = result['distance'][1:].max()
+    min_distance_from_query = result['distance'][1:].min()
+    
+    num_candidates_history.append(num_candidates)
+    query_time_history.append(query_time)
+    average_distance_from_query_history.append(average_distance_from_query)
+    max_distance_from_query_history.append(max_distance_from_query)
+    min_distance_from_query_history.append(min_distance_from_query)
+
+    print average_distance_from_query
+
+plt.figure(figsize=(7,4.5))
+plt.plot(num_candidates_history, linewidth=4)
+plt.xlabel('Search radius')
+plt.ylabel('# of documents searched')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+plt.figure(figsize=(7,4.5))
+plt.plot(query_time_history, linewidth=4)
+plt.xlabel('Search radius')
+plt.ylabel('Query time (seconds)')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+plt.figure(figsize=(7,4.5))
+plt.plot(average_distance_from_query_history, linewidth=4, label='Average of 10 neighbors')
+plt.plot(max_distance_from_query_history, linewidth=4, label='Farthest of 10 neighbors')
+plt.plot(min_distance_from_query_history, linewidth=4, label='Closest of 10 neighbors')
+plt.xlabel('Search radius')
+plt.ylabel('Cosine distance of neighbors')
+plt.legend(loc='best', prop={'size':15})
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
---
+++ src/week2/preprocess.py
@@ -0,0 +1,54 @@
+import numpy as np
+import graphlab
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import pairwise_distances
+import time
+from copy import copy
+
+def norm(x):
+    sum_sq=x.dot(x.T)
+    norm=np.sqrt(sum_sq)
+    return(norm)
+
+def sframe_to_scipy(column):
+    # Create triples of (row_id, feature_id, count).
+    x_frame = graphlab.SFrame({'X1':column})
+    
+    # 1. Add a row number.
+    x_frame_row = x_frame.add_row_number()
+    del x_frame
+    # 2. Stack will transform x to have a row for each unique (row, key) pair.
+    x_stack = x_frame_row.stack('X1', ['feature', 'value'])
+    del x_frame_row
+
+    # Map words into integers using a OneHotEncoder feature transformation.
+    f = graphlab.feature_engineering.OneHotEncoder(features=['feature'])
+
+    # We first fit the transformer using the above data.
+    f.fit(x_stack)
+
+    # The transform method will add a new column that is the transformed version
+    # of the 'word' column.
+    x = f.transform(x_stack)
+    del x_stack
+
+    # Get the feature mapping.
+    mapping = f['feature_encoding']
+
+    # Get the actual word id.
+    x['feature_id'] = x['encoded_features'].dict_keys().apply(lambda x: x[0])
+
+    # Create numpy arrays that contain the data for the sparse matrix.
+    i = np.array(x['id'])
+    j = np.array(x['feature_id'])
+    v = np.array(x['value'])
+    width = x['id'].max() + 1
+    height = x['feature_id'].max() + 1
+
+    # Create a sparse matrix.
+    mat = csr_matrix((v, (i, j)), shape=(width, height))
+
+    del x
+    del v, i, j
+
+    return mat, mapping
